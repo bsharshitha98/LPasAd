3e41df872aa666e0d54613d72ffab8183a3b26f8
bsharshitha98 <b.s.harsh29may@gmail.com>
2025-01-22 11:08:51 +0530

    lpAsAd app created

---
+++ lp-as-ad/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.setHeader("Referrer-Policy", "strict-origin");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
